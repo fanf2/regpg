0e93f0b7d94c54df1330d7bd690531e854b36e7a
Tony Finch <dot@dotat.at>
2017-04-03 15:55:57 +0100

    ansible: make the gpg_d plugin independent of regpg

--- ansible/plugins/filter/gpg_d.py
+++ ansible/plugins/filter/gpg_d.py
@@ -1,18 +1,16 @@
+import ansible
 import subprocess
 
-from ansible import errors
-
-def gpg_d(file, pubring=None):
-    if pubring:
-        argv = [ 'regpg', 'decrypt', '-k', pubring, file ]
-    else:
-        argv = [ 'regpg', 'decrypt', file ]
+def gpg_d(file):
     try:
-        output = subprocess.check_output(argv)
-    except CalledProcessError as e:
-        raise errors.AnsibleFilterError('regpg decrypt '+file+' failed: '+e.output)
+        output = subprocess.check_output(
+            ['gpg', '--use-agent', '--batch', '--quiet', '--decrypt', file])
+    except subprocess.CalledProcessError as e:
+        raise ansible.errors.AnsibleFilterError(
+            'gpg --decrypt '+file+' failed: '+e.output)
     if output == "":
-        raise errors.AnsibleFilterError('regpg decrypt '+file+' produced no output')
+        raise ansible.errors.AnsibleFilterError(
+            'gpg --decrypt '+file+' produced no output')
     return output
 
 class FilterModule(object):
