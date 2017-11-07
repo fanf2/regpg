a09e31bb3c767e5ff9341c67a1b0b2375d8e1411
Tony Finch <dot@dotat.at>
2017-11-07 11:12:19 +0000

    util: preen upload

--- util/upload.sh
+++ util/upload.sh
@@ -3,7 +3,7 @@ git push --tags github master
 git push --tags dotat master
 git push --tags uis master
 
-V=$(git describe | sed 's/-g.*//;s/-/./g;s/\./-/')
+V=$(git describe | perl -pe 's{-(\d+)-\w+}{.$1}')
 perl -pi -e 's{regpg-\d+(\.\d+)+}{'$V'}' regpg
 ln -sf README.html index.html
 
