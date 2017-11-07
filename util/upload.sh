fec4d0896aac6cda432df1fab23e79e6e6dca3e2
Tony Finch <dot@dotat.at>
2017-11-07 11:01:53 +0000

    util: accurate version number in bare script download
    Include the number of commits after a release so the
    version number is not the same as a release version
    when the code differs.

--- util/upload.sh
+++ util/upload.sh
@@ -2,7 +2,13 @@
 git push --tags github master
 git push --tags dotat master
 git push --tags uis master
+
+V=$(git describe | sed 's/-g.*//;s/-/./g;s/\./-/')
+perl -pi -e 's{regpg-\d+(\.\d+)+}{'$V'}' regpg
 ln -sf README.html index.html
+
 rsync -ilrt regpg *.html dist doc \
 	chiark:public-html/prog/regpg/
+
+git checkout regpg
 rm -f index.html
