080d670ca2fc433043ff93c3c0c5020e4b880820
Tony Finch <dot@dotat.at>
2017-11-17 15:44:45 +0000

    util: more accurate upload list

--- util/upload.sh
+++ util/upload.sh
@@ -10,7 +10,7 @@ V=$(git describe | perl -pe 's{-(\d+)-\w+}{.$1}')
 perl -pi -e 's{regpg-\d+(\.\d+)+(\.X)?}{'$V'}' regpg
 gpg --detach-sign --armor regpg
 
-rsync -ilrt regpg regpg.asc *.html dist doc logo \
+rsync -ilrt regpg regpg.asc regpg.html index.html dist doc logo \
 	chiark:public-html/prog/regpg/
 
 git checkout regpg
