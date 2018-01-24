bffb5940e434fef2f11ac8a5fccc2236d0e4ea79
Tony Finch <dot@dotat.at>
2018-01-24 18:20:50 +0000

    regpg-0.100.X

--- regpg.pl
+++ regpg.pl
@@ -1710,7 +1710,7 @@ The F<gpg-preload.yml> playbook uses the F<gpg_d> filter like this:
 
 =head1 VERSION
 
-  This is regpg-0.100 <https://dotat.at/prog/regpg/>
+  This is regpg-0.100.X <https://dotat.at/prog/regpg/>
 
   Written by Tony Finch <fanf2@cam.ac.uk> <dot@dotat.at>
   at Cambridge University Information Services
